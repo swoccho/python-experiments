9aa3d9e5c6cf2e51deec24f8194f5833c14ea3bd
swoccho <asikurrs09@gmail.com>
2021-06-12 12:58:52 +0600

    git test commit

---
+++ git_test.py
@@ -0,0 +1,12 @@
+class Friend:
+    def __init__(self,new_name, new_grades):
+        self.name =new_name
+        self.grades = new_grades
+    def average(self):
+        return sum(self.grades) / len(self.grades)
+
+friend_one = Friend("Rafid", [54,46,89,100,100,99])
+friend_two =Friend("Tahmid", [78,98,99,89,96,100])
+
+print(f"The average of {friend_one.name} is {friend_one.average()}")
+print(f"The average of {friend_two.name} is {friend_two.average()}")
