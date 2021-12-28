a06487a9bd8a5b92016aedad5033b5c746d145fa
swoccho <asikurrs09@gmail.com>
2021-12-28 19:15:23 +0600

    some func updated

--- Book collection/utils/database.py
+++ Book collection/utils/database.py
@@ -1,7 +1,6 @@
-
+import sqlite3
 
 from .database_connection import DatabaseConnection
-
 
 
 def create_book_file():
@@ -11,17 +10,13 @@ def create_book_file():
         cursor.execute('CREATE TABLE IF NOT EXISTS books(name text primary key , author text , read text)')
 
 
-
-
-
-def add_books(name,author):
+def add_books(name, author):
     with DatabaseConnection("data.db") as connection:
-        cursor =connection.cursor()
+        cursor = connection.cursor()
         try:
-            cursor.execute('INSERT INTO books VALUES(?,?,?)',(name, author,"not read"))
-        except :
+            cursor.execute('INSERT INTO books VALUES(?,?,?)', (name, author, "not read"))
+        except sqlite3.IntegrityError:
             print("you already have a book of that name")
-
 
 
 def get_all_books():
@@ -29,8 +24,7 @@ def get_all_books():
         cursor = connection.cursor()
 
         cursor.execute('SELECT * FROM books')
-        books = [{"name":row[0] , "author": row[1] , "read": row[2] } for row in cursor.fetchall()]
-
+        books = [{"name": row[0], "author": row[1], "read": row[2]} for row in cursor.fetchall()]
 
         return books
 
@@ -39,13 +33,18 @@ def mark_as_read(name):
     with DatabaseConnection("data.db") as connection:
         cursor = connection.cursor()
 
-        cursor.execute('UPDATE books SET read="Completed" WHERE name= ?' , (name,))
-
+        cursor.execute('UPDATE books SET read="Completed" WHERE name= ?', (name,))
 
 
 def remove_book(name):
-
     with DatabaseConnection("data.db") as connection:
         cursor = connection.cursor()
 
-        cursor.execute('DELETE FROM books  WHERE name= ?' , (name,))
+        cursor.execute('DELETE FROM books  WHERE name= ?', (name,))
+
+
+def remove():
+    with DatabaseConnection("data.db") as connection:
+        cursor = connection.cursor()
+
+        cursor.execute("DELETE FROM books")
