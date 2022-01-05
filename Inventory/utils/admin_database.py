871a291a7fe6185b8a606c2413a7dac4f3617a66
swoccho <84260820+swoccho@users.noreply.github.com>
2022-01-05 15:17:33 +0600

    admin panel added

--- Inventory/utils/admin_database.py
+++ Inventory/utils/admin_database.py
@@ -1,6 +1,5 @@
 import sqlite3
-import smtplib
-from email.message import EmailMessage
+
 
 def admin_create_table():
     connection = sqlite3.connect("admin.db")
@@ -30,29 +29,5 @@ def admin_data():
 def default_user():
     return "admin"
 
-#
-# def admin_pass_recovery(email,otp):
-#
-#     email_ =EmailMessage()
-#     email_["Subject"] = "Password Recovery"
-#     email_["From"] = gmail,
-#     email_["To"] = email
-#
-#     email_.set_content(f"Your password recovery OTP is {otp}")
-#
-#
-#     smtp_connection = smtplib.SMTP("smtp.gmail.com",port=587)
-#     smtp_connection.starttls()
-#     smtp_connection.login(gmail,password)
-#     smtp_connection.send_message(email_)
-#     smtp_connection.quit()
-#     print("OTP sent to your E-mail..... Please check your E-mail")
-#
-#
-# def recovery_pass_update(new_pass,username):
-#     connection = sqlite3.connect("admin.db")
-#     cursor= connection.cursor()
-#     cursor.execute("UPDATE accounts SET password =? WHERE username =?",(new_pass,username,))
-#     connection.commit()
-#     connection.close()
+
 
