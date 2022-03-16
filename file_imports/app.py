96401c72dfb4d6de333d6c28376ba0012251f191
swoccho <asikurrs09@gmail.com>
2022-03-16 19:46:47 +0600

    updated

--- file_imports/app.py
+++ file_imports/app.py
@@ -6,16 +6,28 @@ class Calculator:
         return Addition.add(num1, num2)
 
     @classmethod
+    def subtract(cls,num1,num2):
+
+        return cls.add(num1,-num2)
+
+    @classmethod
     def multiply(cls,num1,num2):
-        return Addition.multiply(num1,num2)
+        result = 0
+        for x in range(0, num2):
+            result = cls.add(result,num1)
+        return result
 
     @classmethod
     def divide(cls,num1,num2):
-        return Addition.divide(num1,num2)
+        result = 0
+        while num1 >= num2:
+            num1 = cls.subtract(num1,num2)
+            result = cls.add(result,1)
 
-    @classmethod
-    def subtract(cls,num1,num2):
-        return Addition.subtract(num1,num2)
+        return result
+
+
+
 
 
 
